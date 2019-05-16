e7c9b9e481ae1e011feabd097c39d03c8fc3a8c9
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2019-05-16 15:25:28 +0900

    영화 스케줄 크롤링
    현재 날짜 업데이트, 크롤링 된 데이터를 for문에 넣는 문제 겪는중

---
+++ crawl.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import string
+
+driver = webdriver.Chrome('/Users/hansn/Documents/chromedriver')
+driver.implicitly_wait(3)
+driver.get('http://m.cgv.co.kr/Schedule/?tc=0056&t=T&ymd=20190516&src=')
+time.sleep(1)
+
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+cinema_list = []
+movie = []
+save_clist =[]
+cinema_list=soup.select('#theaterList_01 > ul > li')
+movie = soup.select('#divContent > section > h3')
+screen = soup.select('#divContent > section > div > ul > li > a')
+s_time = soup.select('#divContent > section > div > ul > li > a')
+for top1 in cinema_list:
+    save_clist = top1.text
+for topc in cinema_list:
+    for top in s_time:
+        temp
+        print (temp)
+        str1 = ''.join(str(e) for e in top)
+        s_time_2=temp.split('(')
+        s_time_3 = s_time_2[1].split(',')
+        print(topc.text + ' ' + s_time_3[0] + ' ' + s_time_3[17] + ' ' + s_time_3[1] + ' ' + s_time_3[2])
+
+
