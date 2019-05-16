b33d75302d096d3812c75f0a1b6f8805cd05b936
Song MinJoon <33647585+sh05158@users.noreply.github.com>
2019-05-16 22:34:56 +0900

    실행할 때 마다 DB timetable 초기화 후 업데이트

--- crawl.py
+++ crawl.py
@@ -1,3 +1,5 @@
+import pymysql
+
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
@@ -5,15 +7,41 @@ import re
 import string
 from datetime import datetime
 
+db = pymysql.connect(host='lookvies.c4gfbjjoxspj.us-east-2.rds.amazonaws.com',port=3306,user='lookvie',passwd='lookvie1234',db='lookvie',charset='utf8')
+
+cursor = db.cursor()
+
+sql = '''
+        DROP TABLE timetable
+        '''
+
+cursor.execute(sql)
+
+sql = '''
+         CREATE TABLE timetable (
+  pri_key INT NOT NULL,
+  cinema VARCHAR(35) NOT NULL,
+  movie VARCHAR(35) NOT NULL,
+  screen VARCHAR(35) NOT NULL,
+  time VARCHAR(35) NOT NULL,
+  PRIMARY KEY (pri_key)
+);
+        '''
+
+cursor.execute(sql)
+
+cursor.execute('''ALTER TABLE timetable MODIFY COLUMN cinema VARCHAR(35) CHARACTER SET utf8 COLLATE utf8_general_ci''')
+cursor.execute('''ALTER TABLE timetable MODIFY COLUMN movie VARCHAR(35) CHARACTER SET utf8 COLLATE utf8_general_ci''')
+cursor.execute('''ALTER TABLE timetable MODIFY COLUMN screen VARCHAR(35) CHARACTER SET utf8 COLLATE utf8_general_ci''')
 if datetime.today().month<10:
     tempmonth='0'+str(datetime.today().month)
 else:
     tempmonth=str(datetime.today().month)
 
 if datetime.today().day<10:
-    tempday='0'+str(datetime.today().day)
+    tempday='0'+str(datetime.today().day+1)
 else:
-    tempday=str(datetime.today().day)
+    tempday=str(datetime.today().day+1)
 
 today=str(datetime.today().year)+tempmonth+tempday
 
@@ -25,6 +53,7 @@ driver.implicitly_wait(3)
 driver.get('http://m.cgv.co.kr/Schedule/?tc=0056&t=T&ymd='+today+'&src=')
 time.sleep(1)
 tempnum = 0
+pri_key = 0
 for cinemaname in cinemalist:
 
     html = driver.page_source
@@ -38,7 +67,15 @@ for cinemaname in cinemalist:
             s_time0_2 = str0.split('(', maxsplit=1)
             s_time0_3 = s_time0_2[1].split(',')
             s_temp0 = s_time0_3[17].strip('\'\'') + ' ' +  s_time0_3[1].strip('\'\'') + '\''
+            
+            s_time0_3[0]=s_time0_3[0].split("'")[1]
+            s_temp0=s_temp0.split("'")[1]
+            s_time0_3[2]=s_time0_3[2].split("'")[1]
+
             print('CGV'+cinemaname + ' ' + s_time0_3[0] + ' ' + s_temp0 + ' ' + s_time0_3[2])
+            print(s_time0_3[0])
+            cursor.execute("""INSERT INTO timetable (pri_key,cinema,movie,screen,time) VALUES(%s,%s,%s,%s,%s)""",(pri_key,"CGV"+cinemaname,s_time0_3[0],s_temp0,s_time0_3[2]))
+            pri_key=pri_key+1
     numbers = 0
     if tempnum==29:
         break
@@ -46,3 +83,7 @@ for cinemaname in cinemalist:
     driver.find_element_by_xpath('//*[@id="theaterList_01"]/ul/li['+str(tempnum+2)+']/a').click()
     tempnum= tempnum+1
     time.sleep(1)
+
+db.commit()
+
+db.close()
