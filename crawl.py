4fcef497890fca51b15a9f56dbf049cd4fb9474d
HanSolo.CAU <34000255+hansnam1105@users.noreply.github.com>
2019-05-16 17:13:46 +0900

    버그 찾으면 알려주세요. 일단 완성
    Signed-off-by: ROSELOVER <hansnam123@gmail.com>

--- crawl.py
+++ crawl.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
-import string
+import re
 
 driver = webdriver.Chrome('/Users/hansn/Documents/chromedriver')
 driver.implicitly_wait(3)
@@ -12,20 +12,18 @@ time.sleep(1)
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 cinema_list = []
-movie = []
-save_clist =[]
 cinema_list=soup.select('#theaterList_01 > ul > li')
-movie = soup.select('#divContent > section > h3')
 screen = soup.select('#divContent > section > div > ul > li > a')
 s_time = soup.select('#divContent > section > div > ul > li > a')
-for top1 in cinema_list:
-    save_clist = top1.text
 for topc in cinema_list:
     for top in s_time:
         temp = top
-        str1 = str(temp).strip('<>')
-        s_time_2=str1.split('(')
-        s_time_3 = s_time_2[1].split(',')
-        print(topc.text + ' ' + s_time_3[0] + ' ' + s_time_3[17] + ' ' + s_time_3[1] + ' ' + s_time_3[2])
+     #   print(temp)
+        numbers = re.findall("\d+", str(temp))
+        if '20190516' in numbers:
+            str1 = str(temp).strip('<>')
+            s_time_2=str1.split('(')
+            s_time_3 = s_time_2[1].split(',')
+            print(topc.text + ' ' + s_time_3[0] + ' ' + s_time_3[17] + ' ' + s_time_3[1] + ' ' + s_time_3[2])
 
 
